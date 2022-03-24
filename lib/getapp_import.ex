947fbe3fd622a46551c738208626920cb5fd41ba
GuillemAcero <guillemacero@gmail.com>
2022-03-24 15:52:09 +0100

    Finished functionality of the script and added missing tests

--- lib/getapp_import.ex
+++ lib/getapp_import.ex
@@ -5,50 +5,23 @@ defmodule GetappImport do
   @accepted_providers ["capterra", "softwareadvice"]
 
   def main(args) do
-    case validate_arguments(args) do
-      {:error, _} = error ->
-        error
-
-      {:ok, {provider, path}} ->
-        read_file(provider, path)
+    with {:ok, {provider, path}} <- validate_arguments(args),
+         {:ok, result} <- read_file(provider, path) do
+      IO.inspect("Final Result")
+      IO.inspect(result)
+      {:ok, result}
+    else
+      error -> error |> IO.inspect
     end
   end
 
-  def validate_arguments(args) do
+  ### PRIV
+  defp validate_arguments(args) do
     args
     |> validate_length()
     |> validate_provider()
     |> validate_path()
   end
-
-  def read_file(:capterra, path) do
-    [products] = YamlElixir.read_all_from_file!(path)
-
-    try do
-      Enum.map(products, &format_yaml(&1)) |> IO.inspect()
-    rescue
-      File.Error -> {:error, :file_bad_structure} |> IO.inspect()
-    end
-  end
-
-  def read_file(:softwareadvice, path) do
-  end
-
-  defp format_yaml(%{
-         "name" => name,
-         "tags" => tag,
-         "twitter" => twitter
-       }) do
-    %{
-      name: name,
-      categories: tag,
-      twitter: "@" <> twitter
-    }
-  end
-
-  defp format_yaml(_), do: raise(File.Error)
-
-  ### PRIV
 
   defp validate_length([provider, path]), do: {:ok, {provider, path}}
   defp validate_length(_), do: {:error, :invalid_number_of_arguments}
@@ -72,4 +45,72 @@ defmodule GetappImport do
   defp validate_path({:ok, _}), do: {:error, :invalid_path_type}
 
   defp validate_path({:error, _} = error), do: error
+
+  # Same as validate_arguments/1, could/should be private.
+  defp read_file(:capterra, path) do
+    if String.ends_with?(path, ".yaml") do
+      [products] = YamlElixir.read_all_from_file!(path)
+
+      try do
+        {:ok, Enum.map(products, &format_yaml(&1))}
+      rescue
+        File.Error -> {:error, :file_bad_structure}
+      end
+    else
+      {:error, :file_bad_extension}
+    end
+  end
+
+  defp read_file(:softwareadvice, path) do
+    if String.ends_with?(path, ".json") do
+      data =
+        path
+        |> File.read!()
+        |> Jason.decode!()
+
+      try do
+        {:ok, Enum.map(Map.get(data, "products"), &format_json(&1))}
+      rescue
+        File.Error -> {:error, :file_bad_structure}
+      end
+    else
+      {:error, :file_bad_extension}
+    end
+  end
+
+  # As I dont have mote context, let's asume that this 3 fields ARE required
+  defp format_yaml(%{
+         "name" => name,
+         "tags" => tag,
+         "twitter" => twitter
+       }) do
+    %{
+      name: name,
+      categories: tag,
+      twitter: "@" <> twitter
+    }
+  end
+
+  defp format_yaml(_), do: raise(File.Error)
+
+  # In that case one product have twitter key missing, lets asume twitter is optional but the other
+  # two keys are required
+  defp format_json(
+         %{
+           "title" => name,
+           "categories" => categories
+         } = product
+       ) do
+    %{
+      name: name,
+      categories: categories,
+      twitter: format_twitter_name(Map.get(product, "twitter", nil))
+    }
+  end
+
+  defp format_json(_), do: raise(File.Error)
+
+  defp format_twitter_name(nil), do: nil
+
+  defp format_twitter_name(name), do: "@" <> name
 end
--- test/getapp_import_test.exs
+++ test/getapp_import_test.exs
@@ -1,44 +1,64 @@
 defmodule GetappImportTest do
   use ExUnit.Case
 
-  describe "validate_arguments/1" do
-    test "returns ok when params are valid" do
-      assert {:ok, _} =
-               GetappImport.validate_arguments(["capterra", "priv/feed_products/capterra.yaml"])
-    end
-
-    test "returns error when number of arguments invalid" do
-      assert {:error, :invalid_number_of_arguments} == GetappImport.validate_arguments("arg1")
+  describe "main/1" do
+    test "error when number of arguments invalid" do
+      assert {:error, :invalid_number_of_arguments} == GetappImport.main("arg1")
 
       assert {:error, :invalid_number_of_arguments} ==
-               GetappImport.validate_arguments(["arg1", "arg2", "arg3"])
+               GetappImport.main(["arg1", "arg2", "arg3"])
     end
 
-    test "retruns error when the provider is invalid" do
-      assert {:error, :invalid_provider} == GetappImport.validate_arguments(["invalid", "path"])
-      assert {:error, :invalid_provider} == GetappImport.validate_arguments([:invalid, "path"])
-      assert {:error, :invalid_provider} == GetappImport.validate_arguments([1234, "path"])
+    test "error when the provider is invalid" do
+      assert {:error, :invalid_provider} == GetappImport.main(["invalid", "path"])
+      assert {:error, :invalid_provider} == GetappImport.main([:invalid, "path"])
+      assert {:error, :invalid_provider} == GetappImport.main([1234, "path"])
     end
 
-    test "returns error if file path is invalid" do
+    test "error if file path is invalid" do
       assert {:error, :invalid_path_type} ==
-               GetappImport.validate_arguments(["capterra", :not_a_path])
+               GetappImport.main(["capterra", :not_a_path])
 
-      assert {:error, :invalid_path_type} == GetappImport.validate_arguments(["capterra", 12345])
+      assert {:error, :invalid_path_type} == GetappImport.main(["capterra", 12345])
     end
 
-    test "returns error if file does not exist" do
+    test "error if file does not exist" do
       assert {:error, :file_not_found} ==
-               GetappImport.validate_arguments(["capterra", "priv/not_found"])
+               GetappImport.main(["capterra", "priv/not_found"])
 
-      assert {:error, :file_not_found} == GetappImport.validate_arguments(["capterra", ""])
+      assert {:error, :file_not_found} == GetappImport.main(["capterra", ""])
     end
-  end
 
-  describe "read_file/2" do
-    @tag :wip
-    test "test" do
-      GetappImport.read_file(:capterra, "priv/feed_products/capterra.yaml")
+    test "error if file for provider capterra is not yaml" do
+      assert {:error, :file_bad_extension} ==
+               GetappImport.main(["capterra", "priv/test_products/invalid.ex"])
+    end
+
+    test "error if file for provider softwareadvice is json" do
+      assert {:error, :file_bad_extension} ==
+               GetappImport.main(["softwareadvice", "priv/test_products/invalid.ex"])
+    end
+
+    test "error if yaml file have bad structure" do
+      assert {:error, :file_bad_structure} ==
+               GetappImport.main(["capterra", "priv/test_products/invalid.yaml"])
+    end
+
+    test "error if json file have bad structure" do
+      assert {:error, :file_bad_structure} ==
+               GetappImport.main(["softwareadvice", "priv/test_products/invalid.json"])
+    end
+
+    test "returns a list of products if everything's ok (capterra)" do
+      assert {:ok, list} = GetappImport.main(["capterra", "priv/feed_products/capterra.yaml"])
+      assert length(list) == 3
+    end
+
+    test "returns a list of products if everything's ok (softwareadvice)" do
+      assert {:ok, list} =
+               GetappImport.main(["softwareadvice", "priv/feed_products/softwareadvice.json"])
+
+      assert length(list) == 2
     end
   end
 end
