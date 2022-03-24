50577512b6515170848a8a20de710efa610da1c0
GuillemAcero <guillemacero@gmail.com>
2022-03-24 13:00:31 +0100

    Project Start

---
+++ lib/getapp_import.ex
@@ -0,0 +1,6 @@
+defmodule GetappImport do
+  @moduledoc """
+  Documentation for `GetappImport`.
+  """
+
+end
---
+++ test/getapp_import_test.exs
@@ -0,0 +1,8 @@
+defmodule GetappImportTest do
+  use ExUnit.Case
+  doctest GetappImport
+
+  test "greets the world" do
+    assert GetappImport.hello() == :world
+  end
+end
