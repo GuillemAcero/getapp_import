669b7fb503d64f1233fbde9332508774dae98922
GuillemAcero <guillemacero@gmail.com>
2022-03-24 13:56:32 +0100

    Added functions and validations
    In the main module (GetappImport) added the main function along with some validatons for the parameters
    and started the reading file functions. Still WIP

--- lib/getapp_import.ex
+++ lib/getapp_import.ex
@@ -2,5 +2,77 @@ defmodule GetappImport do
   @moduledoc """
   Documentation for `GetappImport`.
   """
+  @accepted_providers ["capterra", "softwareadvice"]
 
+  def main(args) do
+    case validate_arguments(args) do
+      {:error, _} = error -> error
+
+      {:ok, {provider, path}} ->
+        read_file(provider, path)
+
+    end
+
+
+  end
+
+  def validate_arguments(args) do
+    args
+    |> validate_length()
+    |> validate_provider()
+    |> validate_path()
+  end
+
+  def read_file(:capterra, path) do
+    [products] = YamlElixir.read_all_from_file!(path)
+
+    try do
+      Enum.map(products, & format_yaml(&1)) |> IO.inspect
+    rescue
+      File.Error -> {:error, :file_bad_structure} |> IO.inspect
+    end
+  end
+
+  def read_file(:softwareadvice, path) do
+
+  end
+
+  defp format_yaml(%{
+    "name" => name,
+    "tags" => tag,
+    "twitter" => twitter
+  }) do
+    %{
+      name: name,
+      categories: tag,
+      twitter: "@" <> twitter
+    }
+  end
+
+  defp format_yaml(_), do: raise File.Error
+
+  ### PRIV
+
+  defp validate_length([provider, path]), do: {:ok, {provider, path}}
+  defp validate_length(_), do: {:error, :invalid_number_of_arguments}
+
+  defp validate_provider({:ok, {provider, file_path}}) when provider in @accepted_providers do
+    {:ok, {String.to_atom(provider), file_path}}
+  end
+
+  defp validate_provider({:ok, _}), do: {:error, :invalid_provider}
+
+  defp validate_provider({:error, _} = error), do: error
+
+  defp validate_path({:ok, {provider, path}}) when is_binary(path) do
+    if File.exists?(path) do
+      {:ok, {provider, path}}
+    else
+      {:error, :file_not_found}
+    end
+  end
+
+  defp validate_path({:ok, _}), do: {:error, :invalid_path_type}
+
+  defp validate_path({:error, _} = error), do: error
 end
--- test/getapp_import_test.exs
+++ test/getapp_import_test.exs
@@ -1,8 +1,38 @@
 defmodule GetappImportTest do
   use ExUnit.Case
-  doctest GetappImport
 
-  test "greets the world" do
-    assert GetappImport.hello() == :world
+  describe "validate_arguments/1" do
+    test "returns ok when params are valid" do
+      assert {:ok, _} = GetappImport.validate_arguments(["capterra", "priv/feed_products/capterra.yaml"])
+    end
+
+    test "returns error when number of arguments invalid" do
+      assert {:error, :invalid_number_of_arguments} == GetappImport.validate_arguments("arg1")
+      assert {:error, :invalid_number_of_arguments} == GetappImport.validate_arguments(["arg1", "arg2", "arg3"])
+    end
+
+    test "retruns error when the provider is invalid" do
+      assert {:error, :invalid_provider} == GetappImport.validate_arguments(["invalid", "path"])
+      assert {:error, :invalid_provider} == GetappImport.validate_arguments([:invalid, "path"])
+      assert {:error, :invalid_provider} == GetappImport.validate_arguments([1234, "path"])
+    end
+
+    test "returns error if file path is invalid" do
+      assert {:error, :invalid_path_type} == GetappImport.validate_arguments(["capterra", :not_a_path])
+      assert {:error, :invalid_path_type} == GetappImport.validate_arguments(["capterra", 12345])
+    end
+
+    test "returns error if file does not exist" do
+      assert {:error, :file_not_found} == GetappImport.validate_arguments(["capterra", "priv/not_found"])
+      assert {:error, :file_not_found} == GetappImport.validate_arguments(["capterra", ""])
+    end
   end
+
+  describe "read_file/2" do
+    @tag :wip
+    test "test" do
+      GetappImport.read_file(:capterra, "priv/feed_products/capterra.yaml")
+    end
+  end
+
 end
